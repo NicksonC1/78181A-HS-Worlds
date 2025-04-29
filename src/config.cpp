9a431feeb0f14c9ac88671b08af1e9241520fe03
Nickson Chen <540037@ibsh.tw>
2025-04-29 19:57:27 +0800

    before

--- src/config.cpp
+++ src/config.cpp
@@ -78,20 +78,20 @@ genesis::ControllerSettings linearController (6, // proportional gain (kP)
                                               3.75, // derivative gain (kD)
                                               0, // anti windup
                                               1, // small error range, in inches
-                                              100, // small error range timeout, in milliseconds
+                                              50, // small error range timeout, in milliseconds
                                               3, // large error range, in inches
-                                              500, // large error range timeout, in milliseconds
-                                              40 // maximum acceleration (slew)
+                                              250, // large error range timeout, in milliseconds
+                                              60 // maximum acceleration (slew)
 );
 
 genesis::ControllerSettings angularController(3, // proportional gain (kP)
                                               0, // integral gain (kI)
                                               18, // derivative gain (kD) 
                                               0, // anti windup
-                                              0, // small error range, in inches
-                                              0, // small error range timeout, in milliseconds
-                                              0, // large error range, in inches
-                                              0, // large error range timeout, in milliseconds
+                                              1, // small error range, in inches
+                                              50, // small error range timeout, in milliseconds
+                                              3, // large error range, in inches
+                                              250, // large error range timeout, in milliseconds
                                               0 // maximum acceleration (slew)
 );
 
